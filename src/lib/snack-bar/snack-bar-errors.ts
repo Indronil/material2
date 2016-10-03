962a1935fdab8138b9f24e6d8b341a966f50a8cf
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2016-10-03 22:29:33 +0100

    'gh-pages'

--- src/lib/snack-bar/snack-bar-errors.ts
+++
@@ -1,8 +0,0 @@
-import {MdError} from '../core';
-
-
-export class MdSnackBarContentAlreadyAttached extends MdError {
-  constructor() {
-      super('Attempting to attach snack bar content after content is already attached');
-  }
-}
